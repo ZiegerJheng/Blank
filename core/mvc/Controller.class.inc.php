d720003d00d79784459d82195cf7f58fcfd7257d
ZiegerJheng <shjheng@gmail.com>
2014-03-07 02:38:02 +0800

    Controler outline

--- core/mvc/Controller.class.inc.php
+++ core/mvc/Controller.class.inc.php
@@ -1,25 +1,35 @@
 <?php
 class Controller
 {
-    private $do = null;
+    private static $do = null;
+    private static $blankConfigs = array();
 
-    private $modelName;
-    private $modelResult = array();
-
-    private $mvcGeneralConfig;
-
-    private $blankConfigs = array();
-
-    public function __construct()
+    public static function run()
     {
-        $this->mvcGeneralConfig = parse_ini_file(CONFIG_BASE_PATH . 'mvc-general.ini', true);
+        try{
+            self::getDoName();
+            self::loadBlankConfigs();
+            self::doAuth();
+            
+            $model = self::loadModel();
+            $toView = $model::run();
+            
+            self::loadView($toView);
+        }
+        catch(Exception $e){
+            die($e->getMessage());
+        }
     }
 
-    private function loadBlankConfigs()
+    private static function getDoName()
+    {
+    }
+
+    private static function loadBlankConfigs()
     {
         $allRawBlankConfigs = parse_ini_file(BLANKFILE_BASE_PATH . 'blank.ini', true);
-        if(true === array_key_exists('doName', $allRawBlankConfigs)){
-            $thisRawBlankConfigs = $allRawBlankConfigs['doName'];
+        if(true === array_key_exists(self::$do, $allRawBlankConfigs)){
+            $thisRawBlankConfigs = $allRawBlankConfigs[self::$do];
         }
         else{
             $thisRawBlankConfigs = $rawBlankConfigs['default'];
@@ -28,7 +38,7 @@ class Controller
         foreach($thisRawBlankConfigs as $key => $value){
             $configLevels = explode('.', $key);
             $numOfConfigLevels = count($configLevels);
-            $pointer =& $this->blankConfigs;
+            $pointer =& self::$blankConfigs;
             for($index = 0; $index < $numOfConfigLevels; $index++){
                 if(false == array_key_exists($configLevels[$index], $pointer)){
                     $pointer[$configLevels[$index]] = array();
@@ -42,68 +52,8 @@ class Controller
         }
     }
 
-    private function setModelName()
+    private static function loadView($toView)
     {
-        $do = $_GET['do'];
-        if($do !== false){
-            $this->do = $do;
-        }
-
-        if(null === $this->do){
-            $this->modelName = $this->modelConfig['default']['model_name'];
-        }
-        else{
-            $this->modelName = $this->modelConfig[$this->do]['model_name'];
-        }
-    }
-
-    public function run()
-    {
-        $this->setModelName();
-
-        try{
-            $model = $this->loadModel();
-
-            $model->setInput(array(
-                'name' => '_do_',
-                'value' => $this->do
-            ));
-
-            if($model->check()){
-                $model->run();
-            }
-
-            $this->modelResult = $model->loadResult();
-            $this->loadView();
-        }
-        catch(dbException $e){
-            die($e->getMessage());
-        }
-    }
-
-    private function loadModel()
-    {
-        $modelBasePath = $this->mvcGeneralConfig['general']['model_path'];
-        $modelLibPath = $modelBasePath . $this->modelName . '.class.inc.php';
-        if(file_exists($modelLibPath)){
-            require_once($modelLibPath);
-        }
-
-        if(true == class_exists($this->modelName)){
-            return new $this->modelName();
-        }
-
-    }
-
-    private function loadView()
-    {
-        $viewBasePath = $this->mvcGeneralConfig['general']['view_path'];
-
-        foreach($this->modelResult['data'] as $vName => $vValue){
-            $$vName = $vValue;
-        }
-
-        require_once($viewBasePath . $this->modelResult['view'] . '.php');
     }
 }
 ?>
